7565a3d371068fcf82adfd1a94b265a254e7a80c
michivo <michivo@gmail.com>
2018-05-23 17:37:43 +0200

    add project stubs for final project

---
+++ ue8/BalanceControl/Dummy.cpp
@@ -0,0 +1,5 @@
+class Dummy
+{ // TODO: delete this class as soon as you have some real code. Add your classes to project BalanceControl and BalanceLib!
+public:
+	int x;
+};
---
+++ ue8/BalanceControl/main.cpp
@@ -0,0 +1,10 @@
+#include <wiringPi.h>
+
+
+int main(void)
+{
+	wiringPiSetup();
+
+	// TODO: put your balance simulator code here
+	return 0;
+}
