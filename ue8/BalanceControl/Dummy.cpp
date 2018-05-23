acd18671df3bba465352d26eb602f25838a49ff8
michivo <michivo@gmail.com>
2018-05-23 17:39:35 +0200

    add some comments

--- ue8/BalanceControl/Dummy.cpp
+++ ue8/BalanceControl/Dummy.cpp
@@ -1,5 +1,7 @@
 class Dummy
-{ // TODO: delete this class as soon as you have some real code. Add your classes to project BalanceControl and BalanceLib!
+{ // TODO: delete this class as soon as you have some real code. 
+  // You should first add your code and header files to project BalanceControl, 
+  // then add the existing files to project BalanceLib!
 public:
 	int x;
 };
